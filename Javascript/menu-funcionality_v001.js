aae767aa40729e5a76a0c62d647c627a769585cd
veniciusschreiber <veniciusschreiber@gmail.com>
2025-02-05 15:27:35 -0300

    fontype, menu funcionality and section 2 alingment checked

---
+++ Javascript/menu-funcionality_v001.js
@@ -0,0 +1,30 @@
+document.getElementById("homeBtn").addEventListener("click", function () {
+  document.getElementById("homeSection").scrollIntoView({ behavior: "smooth" });
+});
+document.getElementById("whoWeAreBtn").addEventListener("click", function () {
+  document
+    .getElementById("whoWeAreSection")
+    .scrollIntoView({ behavior: "smooth" });
+});
+document.getElementById("portfolioBtn").addEventListener("click", function () {
+  document
+    .getElementById("portfolioSection")
+    .scrollIntoView({ behavior: "smooth" });
+});
+document.getElementById("bottomHomeBtn").addEventListener("click", function () {
+  document.getElementById("homeSection").scrollIntoView({ behavior: "smooth" });
+});
+document
+  .getElementById("bottomWhoWeAreBtn")
+  .addEventListener("click", function () {
+    document
+      .getElementById("whoWeAreSection")
+      .scrollIntoView({ behavior: "smooth" });
+  });
+document
+  .getElementById("bottomPortfolioBtn")
+  .addEventListener("click", function () {
+    document
+      .getElementById("portfolioSection")
+      .scrollIntoView({ behavior: "smooth" });
+  });
